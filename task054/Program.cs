0d5f7bcec0b535520068c98de2b1df3ef28c75e8
Alex_a <shiotoy@icloud.com>
2023-02-12 23:25:33 +0300

    end task056

---
+++ task054/Program.cs
@@ -0,0 +1,49 @@
+﻿// Урок 8. Двумерные массивы. Продолжение
+// Задача 54: Задайте двумерный массив. Напишите программу, которая 
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+
+Random rnd = new Random();
+Console.WriteLine("Например, задан массив:");
+int[,] arr = new int[rnd.Next(5, 10), rnd.Next(5, 10)];
+
+FillArrayRandom(arr);
+PrintArr(arr);
+// Console.Write("В итоге получается вот такой массив:: ");
+
+
+
+
+
+
+
+void FillArrayRandom(int[,] arr)    // создаем рандомный двумерный массив из целых чисел
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(0, 10);
+        }
+    }
+}
+
+void PrintArr(int[,] arry)  // метод вывода на печать массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arry[i, j],2}");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ task056/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер 
+// строки с наименьшей суммой элементов: 1 строка
+
+
+Random rnd = new Random();
+Console.WriteLine("Например, задан массив:");
+int[,] arr = new int[4, 5];
+FillArrayRandom(arr);
+PrintArr(arr);
+Console.Write($"Строка с наименьшей суммой элементов: {MinSumRows(arr)} строка");
+
+
+int MinSumRows(int[,] arry)    // вычисление суммы каждой строки и минимального ее значения
+{
+    int minSum = int.MaxValue;
+    int count = -1;
+
+    for (int i = 0; i < arry.GetLength(0); i++)
+    {
+        int sum = 0;    // обнуляет сумму после каждой строки
+
+        for (int j = 0; j < arry.GetLength(1); j++)    // суммирует все значения столбцов у одной строки, пока кол-во столбцов не закончится
+        {
+            sum += arr[i, j];
+        }
+        Console.WriteLine($"Cумма {i} строки : {sum}");
+
+
+        if (sum < minSum)   // ищем минималку в сумме
+        {
+            minSum = sum;
+            count++;
+        }
+    }
+    return count;
+}
+
+void FillArrayRandom(int[,] arr)    // создаем рандомный двумерный массив из целых чисел
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(0, 10);
+        }
+    }
+}
+
+void PrintArr(int[,] arry)  // метод вывода на печать массива
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arry[i, j],2}");
+        }
+        Console.WriteLine();
+    }
+}
+
